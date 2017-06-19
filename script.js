5e5dcddf7c36aa3a33158bec6a86b7d40f58dd65
Riza Fahmi <rizafahmi@gmail.com>
2017-06-19 10:27:52 +0700

    Template

---
+++ script.js
@@ -0,0 +1,26 @@
+/* global d3 */
+
+// Our canvas
+const width = 750,
+  height = 300,
+  margin = 20
+marginLeft = 40
+
+// Drawing area
+let svg = d3.select('#results')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+
+// Data reloading
+let reload = () => {
+  // Your data parsing here...
+}
+
+// redraw function
+let redraw = (data) => {
+  // Your data to graph here
+
+}
+
+reload()
---
+++ topscore.js
@@ -0,0 +1,17 @@
+/* global d3 width height */
+
+let fill = d3.scaleOrdinal(d3.schemeCategory20)
+let leaderScale = d3.scaleLinear()
+  .range([5, 40])
+
+const draw = (words) => {
+  // Draw your data here...
+
+}
+
+
+const load = () => {
+  // Load your data here...
+}
+
+load()
