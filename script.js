1fe5bcda73478360b449d9df719b6e934d33ff13
arahito <uarahito@yahoo.com>
2017-06-20 10:30:42 +0700

    add fitur world cloud

--- script.js
+++ script.js
@@ -27,60 +27,42 @@ let reload = () => {
 let redraw = (data) => {
   // Your data to graph here
   const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data.map(dataItem => dataItem.GoalsScored))])
-    .range([0, height])
+    .domain([0, 4])
+    // .domain([0, d3.max(data.map(dataItem => dataItem.GoalsScored))])
+    .range([0, height - marginLeft])
+
+  const xScale = d3.scaleLinear()
+    .domain([0, data.length])
+    .range([0, width - (2*marginLeft)])
 
   const colorScale = d3.scaleLinear()
     .domain([0, d3.max(data.map(dataItem => dataItem.GoalsScored))])
     .range(['peru', 'teal'])
 
-  //add the x Axis
-  // var scaleX = d3.scaleLinear()
-  //   .domain([0, data.length])
-  //   .range([0, width - 100])
-  //
-  // var x_axis = d3.axisBottom()
-  //   .scale(scaleX)
-  //
-  // svg.append("g")
-  //   .attr("transform", `translate(0, ${height-margin})`)
-  //   .call(x_axis)
+  // add the x Axis
+  var scaleX = d3.scaleLinear()
+    .domain([0, data.length])
+    .range([0, width - (2*marginLeft)])
+
+  var x_axis = d3.axisBottom()
+    .scale(scaleX)
+
+  svg.append("g")
+    .attr("transform", `translate(${marginLeft}, ${height-margin})`)
+    .call(x_axis.ticks(data.length))
 
   // add the y Axis
-  // var scaleY = d3.scaleLinear()
-  //   .domain([0, d3.max(data.map(dataItem => dataItem.GoalsAllowed))])
-  //   .range([height - marginLeft, 0])
-  //
-  // var y_axis = d3.axisLeft()
-  //   .scale(scaleY)
-  //
-  // svg.append("g")
-  //   .attr("transform", `translate(0, ${width})`)
-  //   .call(y_axis)
+  var scaleY = d3.scaleLinear()
+    .domain([0, 4])
+    // .domain([0, d3.max(data.map(dataItem => dataItem.GoalsAllowed))])
+    .range([height - marginLeft, 0])
 
-  var xscale = d3.scaleLinear()
-                   .domain([0, data.length])
-                   .range([0, width - 100]);
+  var y_axis = d3.axisLeft()
+    .scale(scaleY)
 
-    var yscale = d3.scaleLinear()
-                   .domain([0, d3.max(data.map(dataItem => dataItem.GoalsAllowed))])
-                   .range([height, 0]);
-
-    var x_axis = d3.axisBottom()
-                   .scale(xscale);
-
-    var y_axis = d3.axisLeft()
-                   .scale(yscale);
-
-    svg.append("g")
-       .attr("transform", "translate(50, 10)")
-       .call(y_axis);
-
-    var xAxisTranslate = height + 10;
-
-    svg.append("g")
-            .attr("transform", "translate(50, " + xAxisTranslate  +")")
-            .call(x_axis)
+  svg.append("g")
+    .attr("transform", `translate(${marginLeft}, ${margin})`)
+    .call(y_axis)
 
   svg.selectAll('rect')
     .data(data)
@@ -88,12 +70,14 @@ let redraw = (data) => {
     .append('rect')
     .attr('class', 'bar')
     .attr('x', (d, index) => {
-      return index * 25
+      return xScale(index) + marginLeft
     })
     .attr('y', (d, index) => {
       return height - yScale(d.GoalsScored) - margin
     })
-    .attr('width', 20)
+    .attr('width', (d) => {
+      return xScale(1) - 2
+    })
     .attr('height', (d) => {
       return yScale(d.GoalsScored)
     })
@@ -102,7 +86,7 @@ let redraw = (data) => {
       d3.select(this).style('fill', '#bada55')
     })
     .on('mouseout', function (d, index) {
-      d3.select(this).style('fill', colorScale(data.map(dataItem => dataItem.GoalsScored)))
+      d3.select(this).style('fill', 'teal')
     })
 
 }
--- topscore.js
+++ topscore.js
@@ -1,17 +1,54 @@
-/* global d3 width height */
+/* global d3 */
+
+// Our canvas
+// var width = 750, height = 500
 
 let fill = d3.scaleOrdinal(d3.schemeCategory20)
 let leaderScale = d3.scaleLinear()
   .range([5, 40])
 
-const draw = (words) => {
-  // Draw your data here...
+d3.layout.cloud()
+  .size([750, 500])
+  .words([
+    "Hello", "world", "normally", "you", "want", "more", "words",
+    "than", "this"].map(function(d) {
+    return {text: d, size: 10 + Math.random() * 90, test: "haha"};
+  }))
+  .padding(5)
+  .rotate(function() { return ~~(Math.random() * 2) * 90; })
+  .font("Impact")
+  .fontSize(function(d) { return d.size; })
+  .on("end", draw)
+  .start();
 
+function draw(words) {
+  // Draw your data here...
+  d3.select("#top-score").append("svg")
+    .attr('width', 750)
+    .attr('height', 500)
+    .append("g")
+    .attr("transform", "translate(" + 750 / 2 + "," + 500 / 2 + ")")
+    .selectAll("text")
+    .data(words)
+    .enter().append("text")
+    .style("font-size", function(d) { return d.size + "px"; })
+    .style("font-family", "Impact")
+    .style("fill", function(d, i) { return fill(i); })
+    .attr("text-anchor", "middle")
+    .attr("transform", function(d) {
+      return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+    })
+    .text(function(d) { return d.text; });
 }
 
 
 const load = () => {
   // Load your data here...
+  d3.tsv('afcw-results.tsv', (rows) => {
+    console.log('ini rows', rows);
+    // redraw(rows)
+    // draw(layout.words)
+  })
 }
 
-load()
+// load()
