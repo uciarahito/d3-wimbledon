23fc008ff2af1f00844ec198f02d3fac9655b772
arahito <uarahito@yahoo.com>
2017-06-20 18:22:12 +0700

    implement world cloud from data in stats.tsv

--- topscore.js
+++ topscore.js
@@ -7,20 +7,6 @@ let fill = d3.scaleOrdinal(d3.schemeCategory20)
 let leaderScale = d3.scaleLinear()
   .range([5, 40])
 
-d3.layout.cloud()
-  .size([750, 500])
-  .words([
-    "Hello", "world", "normally", "you", "want", "more", "words",
-    "than", "this"].map(function(d) {
-    return {text: d, size: 10 + Math.random() * 90, test: "haha"};
-  }))
-  .padding(5)
-  .rotate(function() { return ~~(Math.random() * 2) * 90; })
-  .font("Impact")
-  .fontSize(function(d) { return d.size; })
-  .on("end", draw)
-  .start();
-
 function draw(words) {
   // Draw your data here...
   d3.select("#top-score").append("svg")
@@ -41,14 +27,23 @@ function draw(words) {
     .text(function(d) { return d.text; });
 }
 
-
 const load = () => {
   // Load your data here...
   d3.tsv('afcw-results.tsv', (rows) => {
     console.log('ini rows', rows);
-    // redraw(rows)
     // draw(layout.words)
+    d3.layout.cloud()
+      .size([750, 500])
+      .words(rows.map(function(d) {
+        return {text: d.Opponent, size: 10 + Math.random() * 90, test: "haha"};
+      }))
+      .padding(5)
+      .rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .font("Impact")
+      .fontSize(function(d) { return d.size; })
+      .on("end", draw)
+      .start();
   })
 }
 
-// load()
+load()
